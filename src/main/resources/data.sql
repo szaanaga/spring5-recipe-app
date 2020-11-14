6946128b51ed5961de9c07add5facd21385e46c2
pb00129 <pb00129@pbzciswin10_2.wp.lan>
2020-11-14 12:06:00 +0100

    139

---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+INSERT into category (description) values ('American');
+INSERT into category (description) values ('Italian');
+INSERT into category (description) values ('Mexican');
+INSERT into category (description) values ('Fat Food');
+INSERT into unit_of_measure (description) values ('Teaspoon');
+INSERT into unit_of_measure (description) values ('Tablespoon');
+INSERT into unit_of_measure (description) values ('Cup');
+INSERT into unit_of_measure (description) values ('Pinch');
+INSERT into unit_of_measure (description) values ('Ounce');
